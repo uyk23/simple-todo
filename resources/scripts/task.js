33a8273fdbfde4fc2681f90bb468135061d3e400
uyk23 <143029148+uyk23@users.noreply.github.com>
2023-10-11 17:24:35 -0400

    Add delete task button and function

--- resources/scripts/task.js
+++ resources/scripts/task.js
@@ -6,9 +6,24 @@ const taskList = document.getElementById("task-list");
 function addTaskToPage() {
     let task = document.querySelector("#task-text").value;
     if (task) {
+        let taskDiv = document.createElement("div");
+        taskDiv.style.display = "flex";
+
         let taskItem = document.createElement("li");
         taskItem.className = "task-item white-bg";
         taskItem.innerHTML = task;
-        taskList.appendChild(taskItem);
+
+        let deleteButton = document.createElement("button");
+        deleteButton.addEventListener("click", deleteTask);
+        deleteButton.innerHTML = "Delete";
+        deleteButton.className = "border";
+
+        taskDiv.appendChild(taskItem);
+        taskDiv.appendChild(deleteButton);
+        taskList.appendChild(taskDiv);
     }
+}
+
+function deleteTask() {
+    this.parentElement.remove();
 }
